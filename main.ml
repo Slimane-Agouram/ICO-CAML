96bcad5ffaac74e5f855e6dd3cb195e978a4cab8
Slimane AGOURAM <slimane.agouram@gmail.com>
2013-12-15 01:01:43 +0100

    OK Shit works motherfuckers

--- main.ml
+++ main.ml
@@ -5,7 +5,7 @@
 let main () =
   try
     let ic = open_in Sys.argv.(1) in
-    let lexbuf = Lexing.from_channel stdin in
+    let lexbuf = Lexing.from_channel ic in
     (*while true do*)
       let pgm = Parser.main Lexer.token lexbuf in
       Printf.printf "Parsing OK\n" ; exit 0
@@ -15,21 +15,5 @@ let main () =
 let _ = Printexc.print main ()
 ;;
 
-main();;
-(*
-            
-            let proglistlist=
-    let lexbuf = Lexing.from_channel (open_in "fact3.p") in
-    Parser.main Lexer.token lexbuf;;*)
-    
-    (* file: main.ml 
-let main () =
-  try
-    let lexbuf = Lexing.from_channel stdin in
-    while true do
-      Parser.main Lexer.token lexbuf
-    done
-  with End_of_file -> exit 0
-      
-let _ = Printexc.print main ()
-*)
+main()
+;;
